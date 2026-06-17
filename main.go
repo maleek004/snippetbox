e3a64783a43f3632319795a68fe75e8b03891feb
Abdulmaleek Mubaraq <mubaraqabdulmaleek@gmail.com>
2026-06-17 16:01:38 +0100

    2nd commit

--- main.go
+++ main.go
@@ -7,12 +7,24 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	fmt.Fprint(w, "Welcome to snippetbox")
 }
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("we are going to print a sample snippet here"))
+}
 
+func snippetWrite(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("we are going to create snippets with this route"))
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/write", snippetWrite)
 
 	port := ":4000"
 	log.Printf("Server starting at port %s", port)
